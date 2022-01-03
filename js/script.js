b71f76712ae9c20b02cb0a02fca85cf55aa4f2ac
Vitor <frete497@gmail.com>
2022-01-02 20:03:57 -0400

    pergun

--- js/script.js
+++ js/script.js
@@ -24,3 +24,25 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+/* perguntas */
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+
+/* Galeria bicicletas */
