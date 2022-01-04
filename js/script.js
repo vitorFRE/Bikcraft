4a4b4dd425e6e546f47f01871d2926b8253eff2e
Vitor <frete497@gmail.com>
2022-01-04 15:04:35 -0400

    anim

--- js/script.js
+++ js/script.js
@@ -46,3 +46,26 @@ function eventoPerguntas(pergunta) {
 perguntas.forEach(eventoPerguntas);
 
 /* Galeria bicicletas */
+
+const galeria = document.querySelectorAll(".bicicleta-imgs img");
+const galeriaContainer = document.querySelector(".bicicleta-imgs");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+/* animação */
+
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
